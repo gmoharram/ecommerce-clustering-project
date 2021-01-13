c8ad72496a1b89407253082822510577b74d14f3
gmoharram <31007899+gmoharram@users.noreply.github.com>
2021-01-13 01:29:46 +0100

    Add files via upload

--- DataExploration.py
+++ DataExploration.py
@@ -14,7 +14,7 @@ plt.style.use('ggplot') #choose plot style
 import pickle #to store and access variables in .pckl files
 
 with open('pckl_variables/FeatureMatrix.pckl', 'rb') as f:
-    X = pickle.load(f)
+    [X, columns] = pickle.load(f)
     f.close()
 
 
@@ -47,3 +47,20 @@ ax2.set_ylabel('# of invoices')
 ax2.set_xlabel('# of different items')
 ax2.set_xlim(xmin = 0, xmax = np.max(diff_items_counts[2:220, 0]))  
 ax2.set_ylim(ymin = 0, ymax = np.max(diff_items_counts[2:220,1]))
+
+
+
+##Mean Quantity of item when purchased 
+mean_quantity = np.zeros(X.shape[1]) 
+std_dev_quantity = np.zeros(X.shape[1]) 
+for i in range(X.shape[1]):
+    select = np.array(np.where(X[:,i] != 0))   
+    mean_quantity[i] = np.mean(X[select, i])
+    std_dev_quantity[i] = np.std(X[select, i])
+    
+#PLOTS
+fig3, ax3 = plt.subplots()
+ax3.scatter(np.arange(X.shape[1]), mean_quantity)
+ax3.set_title('Mean Quantities Purchased')
+ax3.set_ylabel('Mean Quantity')
+ax3.set_xlabel('Item Number')
--- SaveInputData.py
+++ SaveInputData.py
@@ -29,6 +29,7 @@ with open('pckl_variables/loaded_baskets_df.pckl', 'rb') as f:
    
 #Create numpy feature matrix out of df
 X = df.to_numpy()
+columns = df.columns.to_numpy() #Associate Column Number with Stock Code
 
 
 ## check if there are items with negative quantities
@@ -43,6 +44,7 @@ X= np.delete(X, negative_indices[:,0], axis = 0)
 ##Remove item that has only been purchased once at a quantity of 80995 and belonging invoice row 
 X = np.delete(X, 20756, axis = 0)
 X = np.delete(X, 4068, axis = 1)
+columns = np.delete(columns, 4068)
 
 
 #Remove empty columns
@@ -51,12 +53,13 @@ for i in range(X.shape[1]):
     if np.count_nonzero(X[:,i]) == 0:
         empty = np.append(empty, i)
 X = np.delete(X, empty, axis = 1)
+columns = np.delete(columns, empty)
 
 
 
 
 #Save Feature Matrix to .pckl file for future use
 with open('pckl_variables/FeatureMatrix.pckl', 'wb') as f:
-    pickle.dump(X, f)
+    pickle.dump([X, columns], f)
     f.close()
     
